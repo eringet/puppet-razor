5be3abc84d8f73289faa4a3de9d8a25ff80fdd27
Danny Roberts <d.roberts@node4.co.uk>
2015-06-23 14:18:39 +0100

    Minor typo in validate check

--- manifests/client.pp
+++ manifests/client.pp
@@ -13,7 +13,7 @@ class razor::client (
   validate_string($package_name)
 
   validate_string($package_ensure)
-  validate_re($package_ensure, '^(present|latest|nstalled|[._0-9a-zA-Z:-]+)$')
+  validate_re($package_ensure, '^(present|latest|installed|[._0-9a-zA-Z:-]+)$')
 
   package { $package_name:
     ensure   => $package_ensure,
